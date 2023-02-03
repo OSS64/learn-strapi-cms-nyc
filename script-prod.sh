6b318d88de9c084fbfe00acd3daaf6468b1f87e4
Jobayerdev <jobayerhossain977@gmail.com>
2023-02-03 19:19:29 +0600

    main: Added

--- script-prod.sh
+++ script-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
+
+sudo npm install -g pm2
 node -v
 npm install
 npm run build
-npm pm2 start pm2.prod.config.json
+pm2 start pm2.prod.config.json
